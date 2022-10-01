bafa7b6aa5a33475ed9c6c1eeed9a6120b50add1
Mefor Louis <mefor.louis@gmail.com>
2022-10-01 06:06:55 +0200

    Added Album model data class

--- app/src/main/java/com/adastra_one/musictime/model/Ablums.kt
+++ app/src/main/java/com/adastra_one/musictime/model/Ablums.kt
@@ -3,3 +3,44 @@ package com.adastra_one.musictime.model
 data class Ablums(
     val feed: Feed
 )
+
+data class Author(
+    val name: String,
+    val url: String
+)
+
+data class Feed(
+    val author: Author,
+    val copyright: String,
+    val country: String,
+    val icon: String,
+    val id: String,
+    val links: List<Link>,
+    val results: List<Result>,
+    val title: String,
+    val updated: String
+)
+
+data class Genre(
+    val genreId: String,
+    val name: String,
+    val url: String
+)
+
+data class Link(
+    val self: String
+)
+
+data class Result(
+    val artistId: String,
+    val artistName: String,
+    val artistUrl: String,
+    val artworkUrl100: String,
+    val contentAdvisoryRating: String,
+    val genres: List<Genre>,
+    val id: String,
+    val kind: String,
+    val name: String,
+    val releaseDate: String,
+    val url: String
+)
